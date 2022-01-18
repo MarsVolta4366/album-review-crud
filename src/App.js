3a22f41248d0e1f12f84354e94acce4d082b8263
unknown <derek.slauson5@gmail.com>
2022-01-18 09:15:20 -0600

    Some stub components are done along with navbar

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
 import './App.css'
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom'
+import "bootstrap/dist/css/bootstrap.min.css"
+// COMPONENTS
 import AlbumsGallery from './components/AlbumsGallery'
 import NewAlbum from './components/NewAlbum'
-import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <header>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Albums</Link>
-              </li>
-              <li>
-                <Link to="/newAlbum">New Album</Link>
-              </li>
-            </ul>
-          </nav>
-        </header>
-
+        <Navbar />
+        <br />
         <Routes>
-          <Route path="/" element={<AlbumsGallery />} />
-          <Route path="/newAlbum" element={<NewAlbum />}/>
+          <Route exact path="/" element={<AlbumsGallery />} />
+          <Route path="/newAlbum" element={<NewAlbum />} />
         </Routes>
       </Router>
     </div>
---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-dark bg-dark navbar-expand-lg p-2">
+        <Link to="/" className="navbar-brand">Album Tracker</Link>
+        <div className="collapse navbar-collapse">
+            <ul className="navbar-nav mr-auto">
+                <li className="navbar-item">
+                    <Link to="/" className="nav-link">Albums</Link>
+                </li>
+                <li className="navbar-item">
+                    <Link to="/newAlbum" className="nav-link">New Album</Link>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    )
+}
+
+export default Navbar
