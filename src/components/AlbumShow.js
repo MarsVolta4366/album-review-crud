82f48be2703ef976b884be5965ae59bcdadbeb4f
unknown <derek.slauson5@gmail.com>
2022-01-24 17:42:03 -0600

    Installed react bootstrap, review form is working, album show page displays reviews

--- src/components/AlbumShow.js
+++ src/components/AlbumShow.js
@@ -1,13 +1,21 @@
 import axios from "axios"
 import { useParams } from "react-router-dom"
 import { useEffect, useState } from "react/cjs/react.development"
+import Form from "react-bootstrap/Form"
+import Button from "react-bootstrap/Button"
 
 export default function AlbumShow() {
 
     const { albumIdParams } = useParams()
+    // Album state variables
     let [name, setName] = useState("")
     let [artist, setArtist] = useState("")
     let [releaseYear, setReleaseYear] = useState()
+    // Review state variables
+    let [author, setAuthor] = useState("")
+    let [stars, setStars] = useState(0)
+    let [content, setContent] = useState("")
+    let [reviews, setReviews] = useState([])
 
     useEffect(() => {
         axios.get(`http://localhost:5000/albums/${albumIdParams}`)
@@ -15,8 +23,37 @@ export default function AlbumShow() {
                 setName(resData.data.name)
                 setArtist(resData.data.artist)
                 setReleaseYear(resData.data.releaseYear)
+                setReviews(resData.data.reviews)
             })
     }, [])
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+        const review = {
+            author: author,
+            stars: stars,
+            content: content,
+            album: albumIdParams
+        }
+        axios.post("http://localhost:5000/reviews/add", review)
+        window.location = `/showAlbum/${albumIdParams}`
+    }
+
+    let reviewsDisplay = (
+        <p>No reviews yet</p>
+    )
+    if (reviews.length > 0) {
+        reviewsDisplay = reviews.map((rev, index) => {
+            return (
+                <div key={index}>
+                    <hr />
+                    <p>Author: {rev.author}</p>
+                    <p>Stars: {rev.stars}</p>
+                    <p>Content: {rev.content}</p>
+                </div>
+            )
+        })
+    }
 
     return (
         <div>
@@ -24,7 +61,27 @@ export default function AlbumShow() {
             <p>Artist: {artist}</p>
             <p>Release Year: {releaseYear}</p>
 
-            {/* Bootstrap add review form, first redo so this project uses react bootstrap */}
+            <h2>Write a Review</h2>
+            <Form>
+                <Form.Group className="mb-3">
+                    <Form.Label>Author</Form.Label>
+                    <Form.Control type="text" placeholder="Author Name" onChange={(e) => setAuthor(e.target.value)} />
+                </Form.Group>
+                <Form.Group className="mb-3">
+                    <Form.Label>Stars</Form.Label>
+                    <Form.Control type="number" placeholder="Stars" onChange={(e) => setStars(e.target.value)} />
+                </Form.Group>
+                <Form.Group className="mb-3">
+                    <Form.Label>Review</Form.Label>
+                    <Form.Control as="textarea" aria-label="With textarea" onChange={(e) => setContent(e.target.value)} />
+                </Form.Group>
+                <Button variant="primary" type="submit" onClick={(e) => onSubmit(e)}>
+                    Submit
+                </Button>
+            </Form>
+
+            <h2>Reviews</h2>
+            {reviewsDisplay}
         </div>
     )
 }
--- src/components/AlbumsGallery.js
+++ src/components/AlbumsGallery.js
@@ -1,4 +1,3 @@
-import "bootstrap/dist/css/bootstrap.min.css"
 import { Link } from "react-router-dom"
 
 const AlbumsGallery = (props) => {
