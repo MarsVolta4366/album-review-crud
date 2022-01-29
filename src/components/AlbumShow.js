18cc99a582efce44bedf38d61d3ed48c51e35b26
unknown <derek.slauson5@gmail.com>
2022-01-28 19:32:29 -0600

    Trying to use navigate to be more consistant

--- src/components/AlbumShow.js
+++ src/components/AlbumShow.js
@@ -38,7 +38,8 @@ export default function AlbumShow(props) {
             album: albumIdParams
         }
         axios.post("https://album-review-crud-backend.herokuapp.com/reviews/add", review)
-        window.location = `/showAlbum/${albumIdParams}`
+            .then(() => navigate(`/showAlbum/${albumIdParams}`))
+        // window.location = `/showAlbum/${albumIdParams}`
     }
 
     let reviewsDisplay = (
