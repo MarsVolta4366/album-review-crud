6c2111ea74c5b03bb8fdd0bdb3f525120105132e
unknown <derek.slauson5@gmail.com>
2022-01-28 19:19:41 -0600

    Trying to fix review post redirect

--- src/components/AlbumShow.js
+++ src/components/AlbumShow.js
@@ -38,7 +38,7 @@ export default function AlbumShow(props) {
             album: albumIdParams
         }
         axios.post("https://album-review-crud-backend.herokuapp.com/reviews/add", review)
-        .then(() => window.location.reload())
+        window.location.reload()
         // window.location = `/showAlbum/${albumIdParams}`
     }
 
