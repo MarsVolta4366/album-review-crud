e9de65e65ee98a9a85b9e2f9a9403da99f098e47
unknown <derek.slauson5@gmail.com>
2022-01-28 18:36:02 -0600

    Took .location out of .then to hopefully fix 404

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+// I think everything is working except add a review
 import './App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import "bootstrap/dist/css/bootstrap.min.css"
